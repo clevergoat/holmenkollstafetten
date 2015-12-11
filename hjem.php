a5280897162a6abdeb636cf9c504f8427904c026
Rikard Andresen <clevergoat@gmail.com>
2015-12-11 16:55:21 +0100

    Hero buttons always visible

--- hjem.php
+++ hjem.php
@@ -6,70 +6,70 @@ Template Name: Hjem
 ?>
 
 <?php get_header(); ?>
-	
-	<div class="container">
-	<div class="logo columns float-center" style="text-align: center; padding: 50px 0;">
-		<img src="/wp-content/uploads/hk_logo.png">
+
+<div class="container">
+	<div class="logo">
+		<img src="/hks/wp-content/uploads/hk_logo.png">
 	</div>
-	<div class="show-for-medium" style="margin-bottom: 50px; height: 800px; background: url(/wp-content/uploads/bakgrunn.jpg); background-position: center; background-size: cover; background-repeat: no-repeat;">
-			
+	<div class="show-for-medium hoved-bg">
 	</div>
 
-
+	<div class="inner-container">
+		<div class="row">
+			<div class="small-12 medium-4 large-4 columns">
+				<a href="https://reg.eqtiming.no/?EventUID=20006" class="hero large expanded button">
+					<h2>Påmelding</h2>
+					<p class="show-for-medium">Meld på ditt lag</p>
+				</a>
 			</div>
+			<div class="small-12 medium-4 large-4 columns">
+				<a href="/hks/deltagerinfo/" class="hero large expanded button">
+					<h2>Deltagerinfo</h2>
+					<p class="show-for-medium">Svarene til alt du lurer på</p>
+				</a>
+			</div>
+			<div class="small-12 medium-4 large-4 columns">
+				<a href="/hks/loypekart/" class="hero large expanded button">
+					<h2>Løypekart</h2>
+					<p class="show-for-medium">Finn din etappe</p>
+				</a>
+			</div></div>
+		</div>
+	</div>
 	<div id="content">
-	
+
 		<div id="inner-content" class="row">
-				<div class="small-12 medium-4 large-4 columns">
-		<a href="https://reg.eqtiming.no/?EventUID=20006" class="hero large expanded button" style="border-bottom: 3px solid lightgray;">
-			<h2>Påmelding</h2>
-			<p class="show-for-medium">Meld på ditt lag</p>
-		</a>
-		</div>
-				<div class="small-12 medium-4 large-4 columns">
-						<a href="/hks/deltagerinfo/" class="hero large expanded button" style="border-bottom: 3px solid lightgray;">
-			<h2>Deltagerinfo</h2>
-			<p class="show-for-medium">Svarene til alt du lurer på</p>
-		</a>
-		</div>
-				<div class="small-12 medium-4 large-4 columns">
-						<a href="/hks/loypekart/" class="hero large expanded button" style="border-bottom: 3px solid lightgray;">
-			<h2>Løypekart</h2>
-			<p class="show-for-medium">Finn din etappe</p>
-		</a>
-		</div>
 
-	
-		<main id="main" class="large-12 medium-12 columns" role="main" style="margin: 30px 0 40px;">
+			<main id="main" class="large-12 medium-12 columns" role="main" style="margin: 30px 0 40px;">
 
-			<?php 
+				<?php 
 				$args = array( 'posts_per_page' => '3' );
 				$recent_posts = new WP_Query( $args ); 
-			?>
+				?>
 
-			<?php while ( $recent_posts->have_posts() ) : $recent_posts->the_post(); ?>
-			
-			<?php get_template_part( 'parts/loop', 'archive' ); ?>
-			
-			<?php endwhile; // end of the loop. ?>
-			
-			<?php wp_reset_postdata(); ?>
-			
+				<?php while ( $recent_posts->have_posts() ) : $recent_posts->the_post(); ?>
 
-																								
-		    </main> <!-- end #main -->
+					<?php get_template_part( 'parts/loop', 'archive' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+				<?php wp_reset_postdata(); ?>
 
 
-		    <div class="small-12 columns">
-		<a href="/hks/deltagerinfo/" class="hero large expanded button" style="border-bottom: 3px solid lightgray;">
-			<h2>Se flere nyheter</h2>
-		</a>
-		</div>
 
-		    <?php // get_sidebar(); ?>
+			</main> <!-- end #main -->
+
+
+			<div class="small-12 columns">
+				<a href="/hks/deltagerinfo/" class="hero large expanded button" style="border-bottom: 3px solid lightgray;">
+					<h2>Se flere nyheter</h2>
+				</a>
+			</div>
+
+			<?php // get_sidebar(); ?>
 
 		</div> <!-- end #inner-content -->
 
 	</div> <!-- end #content -->
 
-<?php get_footer(); ?>
+	<?php get_footer(); ?>
