0f3a70473596d63c51716db28b232c330dcf0da1
Rikard Andresen <clevergoat@gmail.com>
2016-01-06 15:37:33 +0100

    Add loypekart template

---
+++ loypekart.php
@@ -0,0 +1,30 @@
+
+<?php
+/*
+Template Name: Løypekart
+*/
+?>
+
+<?php get_header(); ?>
+	
+	<div id="content">
+	
+		<div id="inner-content" class="row">
+	
+		    <main id="main" class="large-8 medium-8 columns" role="main">
+				
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+			    	<?php get_template_part( 'parts/loop', 'page' ); ?>
+			    
+			    <?php endwhile; endif; ?>							
+			    					
+			</main> <!-- end #main -->
+
+		    <p>loypetest</p>
+		    
+		</div> <!-- end #inner-content -->
+
+	</div> <!-- end #content -->
+
+<?php get_footer(); ?>
