8179e5897ebf61a6eef11728d17da1428e23c0d3
Rikard Andresen <clevergoat@gmail.com>
2016-01-12 13:55:37 +0100

    Alter hero buttons, timer visibility

--- hjem.php
+++ hjem.php
@@ -7,19 +7,20 @@ Template Name: Hjem
 
 <?php get_header(); ?>
 
-<div class="container">
+<div class="container" style="height: calc(100vh - 90px);">
 	<div class="logo">
 		<img src="<?php echo get_template_directory_uri(); ?>/images/hk_logo.png">
 	</div>
-	<div class="show-for-medium hoved-bg">
-	</div>
-	<div class="hks-timer show-for-medium" style="position: relative; bottom: 225px; margin: 0 auto; text-align: center; max-width: 550px;">
+	<div class="show-for-medium hoved-bg" style="height: 100%; position: static;">
+		<div class="hks-timer show-for-medium" style="position: absolute; left: 0; right: 0; bottom: 50px; margin: 0 auto; text-align: center; width: 550px;">
 		<h3 style="font-weight: 200; text-shadow: 0 1px 0 #fff; ">Norges største friidrettsarrangement.</h3>
 		<h3 style="font-weight: 600;">Lørdag 7. mai, 2016.</h3>
 		<?php echo _e(do_shortcode('[:no][easy_countdowner name="demo_countdowner" theme="default" animation="ticks" end_date="2016-05-07" end_time="00:00:00" day_label="Dager" hour_label="Timer" minute_label="Minutter" second_label="Sekunder"][:en][easy_countdowner name="demo_countdowner" theme="default" animation="ticks" end_date="2016-05-07" end_time="00:00:00" day_label="Days" hour_label="Hours" minute_label="Minutes" second_label="Seconds"[:]'));?>
 		</div>
+	</div>
 
-	<div class="inner-container">
+
+	<div class="inner-container" style="position: static; bottom: inherit;">
 		<div class="row">
 			<div class="small-12 large-4 columns">
 				<a href="https://reg.eqtiming.no/?EventUID=20006" class="hero large expanded button bottom-gray">
